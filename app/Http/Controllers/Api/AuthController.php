7a416772c158ca6092e5d09756477a250e7c0c8f
Bronyrus <bronyruschannel@gmail.com>
2019-08-01 14:11:44 +0300

    Update AuthController.php

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -92,7 +92,7 @@ class AuthController extends ApiBaseController
             $input['phone'] = NULL;
         }
  
-        $tryRegister = User::WhereRaw('email = ' . $input['email'] . ' or phone = ' . $input['phone'] . ' or uid = ' . $input['uid'])->first();
+        $tryRegister = User::WhereRaw('email = "' . $input['email'] . '" or phone = "' . $input['phone'] . '" or uid = "' . $input['uid'] . '"')->first();
 
         if($tryRegister)
         {
