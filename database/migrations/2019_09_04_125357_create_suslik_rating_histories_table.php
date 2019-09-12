f5d7ff170085aa02ab7c87918b5bebd134b98e6d
Bronyrus <bronyruschannel@gmail.com>
2019-09-12 16:26:16 +0300

    Update 2019_09_04_125357_create_suslik_rating_histories_table.php

--- database/migrations/2019_09_04_125357_create_suslik_rating_histories_table.php
+++ database/migrations/2019_09_04_125357_create_suslik_rating_histories_table.php
@@ -13,12 +13,12 @@ class CreateSuslikRatingHistoriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('suslik_rating_histories', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->bigInteger('from_suslik');
-            $table->bigInteger('whom_suslik');
-            $table->enum('type', ['likes', 'dislikes', 'neutrals']);
-            $table->timestamps();
+        // Schema::create('suslik_rating_histories', function (Blueprint $table) {
+        //     $table->bigIncrements('id');
+        //     $table->bigInteger('from_suslik');
+        //     $table->bigInteger('whom_suslik');
+        //     $table->enum('type', ['likes', 'dislikes', 'neutrals']);
+        //     $table->timestamps();
 
             // $table->foreign('from_suslik')->references('id')->on('susliks')
             //     ->onUpdate('cascade');
