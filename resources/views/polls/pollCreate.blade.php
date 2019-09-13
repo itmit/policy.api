60066ebe4564f2054f82db098cf5e05023db5b14
Bronyrus <bronyruschannel@gmail.com>
2019-09-13 12:09:07 +0300

    Update pollCreate.blade.php

--- resources/views/polls/pollCreate.blade.php
+++ resources/views/polls/pollCreate.blade.php
@@ -21,33 +21,18 @@
                 </div>
             </div>
 
-            <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                <label for="password" class="col-md-4 control-label">Пароль</label>
+            <div class="form-group{{ $errors->has('radio') ? ' has-error' : '' }}">
+                <input type="radio" id="unlimited" name="time" value="unlimited">
+                <label for="unlimited">Бессрочно</label>
 
-                <div class="col-md-6">
-                    <input id="password" type="password" class="form-control" name="password" required>
-
-                    @if ($errors->has('password'))
-                        <span class="help-block">
-                            <strong>{{ $errors->first('password') }}</strong>
-                        </span>
-                    @endif
-                </div>
-            </div>
-
-            <div class="form-group">
-                <label for="password-confirm" class="col-md-4 control-label">Повторите пароль</label>
-
-                <div class="col-md-6">
-                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation"
-                           required>
-                </div>
+                <input type="radio" id="limited" name="time" value="limited">
+                <label for="limited">С датой начана и завершения</label>
             </div>
 
             <div class="form-group">
                 <div class="col-md-6 col-md-offset-4">
                     <button type="submit" class="btn btn-primary">
-                        Создать клиента
+                        Создать опрос
                     </button>
                 </div>
             </div>
