d3ca1b1dea44ede44ccbb2522536a906976651b0
Bronyrus <bronyruschannel@gmail.com>
2019-09-13 12:25:01 +0300

    Update adminApp.blade.php

--- resources/views/layouts/adminApp.blade.php
+++ resources/views/layouts/adminApp.blade.php
@@ -10,9 +10,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-    <script src="{{ asset('js/script.js') }}"></script>
-    <script src="{{ asset('js/jquery.mask.js') }}"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
