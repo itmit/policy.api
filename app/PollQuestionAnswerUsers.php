478d0cfbb3d4473311a3da98e77f394c3c5d69c9
Bronyrus <bronyruschannel@gmail.com>
2019-10-07 17:52:02 +0300

    Update PollQuestionAnswerUsers.php

--- app/PollQuestionAnswerUsers.php
+++ app/PollQuestionAnswerUsers.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class PollQuestionAnswerUsers extends Model
 {
-    //
+    /**
+     * @var array
+     */
+    protected $guarded = ['id'];
+
+    /**
+     * @var string
+     */
+    protected $table = 'poll_question_answer_users';
 }
