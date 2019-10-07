b57fd120d8038e38bbe6698123f43983481a356b
Bronyrus <bronyruschannel@gmail.com>
2019-10-07 17:47:02 +0300

    Update PollApiController.php

--- app/Http/Controllers/Api/PollApiController.php
+++ app/Http/Controllers/Api/PollApiController.php
@@ -106,7 +106,7 @@ class PollApiController extends ApiBaseController
                 
                 $answer_id = PollQuestionAnswers::where('uuid', '=', $answer_uuid)->first(['id', 'type']);
                 $uuid = Str::uuid();
-                echo $answer_id->type;
+                echo $answer_id['type'];
                 // if($answer_id->type == 0) // обычный ответ, не другой
                 // {
                 //     PollQuestionAnswerUsers::create([
