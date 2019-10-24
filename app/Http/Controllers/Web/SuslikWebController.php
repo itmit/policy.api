4d6cd1b6e2e6d2fc5ad7dc8ad929ec0ec3709998
Bronyrus <bronyruschannel@gmail.com>
2019-10-24 16:04:36 +0300

    Update SuslikWebController.php

--- app/Http/Controllers/Web/SuslikWebController.php
+++ app/Http/Controllers/Web/SuslikWebController.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Storage;
 use Webpatser\Uuid\Uuid;
 use ZipArchive;
 use SplFileInfo;
+use PhpOffice\PhpSpreadsheet\Reader\Xlsx;
+use PhpOffice\PhpSpreadsheet\IOFactory;
 
 class SuslikWebController extends Controller
 {
@@ -146,6 +148,40 @@ class SuslikWebController extends Controller
         foreach($files as $file)
         {
             $fileType = new SplFileInfo($file);
+
+            // ...
+            // Создаём ридер 
+            $reader = new Xlsx();
+            // Если вы не знаете, какой будет формат файла, можно сделать ридер универсальным:
+            // $reader = IOFactory::createReaderForFile($file);
+            // $reader->setReadDataOnly(true);
+            
+            // Если вы хотите установить строки и столбцы, которые необходимо читать, создайте класс ReadFilter
+            // $reader->setReadFilter( new MyReadFilter(11, 1000, range('B', 'O')) );
+            if($fileType->getExtension() == "xlsx")
+            {
+                // Читаем файл и записываем информацию в переменную
+                $spreadsheet = $reader->load($file);
+                            
+                // Так можно достать объект Cells, имеющий доступ к содержимому ячеек
+                $cells = $spreadsheet->getActiveSheet()->getCellCollection();
+                        
+                // Далее перебираем все заполненные строки (столбцы B - O)
+                for ($row = 10; $row <= $cells->getHighestRow(); $row++){
+                    for ($col = 'B'; $col <= 'O'; $col++) {
+                        // Так можно получить значение конкретной ячейки
+                        return $cells->get($col.$row)->getValue();
+
+                        // а также здесь можно поместить ваш функциональный код
+                    }
+                }            
+
+                return 'true';
+            }
+
+            return 'false';
+            
+
             if($fileType->getExtension() == "csv" || $fileType->getExtension() == "xlsx")
             {
                 $url = storage_path() . '/app/susliks_upload/' . $file;
