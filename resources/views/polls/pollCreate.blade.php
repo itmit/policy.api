a624b1a27d20c127e8fb363c50e990bc01a795a4
Bronyrus <bronyruschannel@gmail.com>
2019-10-29 16:57:28 +0300

    Update pollCreate.blade.php

--- resources/views/polls/pollCreate.blade.php
+++ resources/views/polls/pollCreate.blade.php
@@ -224,8 +224,7 @@
 
             $( ".question" ).each(function( index ) {
                 // console.log($(this).html());
-                // console.log($(this).children($("input[name='question_name']")).val());
-                console.log($("input[name='question_name']").val());
+                console.log($(this).find($("input[name='question_name']").val()));
             });
 
             // for (let pair of data.entries()) {
