968194f0da024ed7b1e023fd27cb0a5c90b7f350
Bronyrus <bronyruschannel@gmail.com>
2019-10-29 11:59:18 +0300

    Update pollCreate.blade.php

--- resources/views/polls/pollCreate.blade.php
+++ resources/views/polls/pollCreate.blade.php
@@ -197,6 +197,7 @@
     </style>
 
     <template id="sergay">
+        <hr>
         <div class="question">
             <div class="question_name col-md-4">
                 <input type="text" name="question_name" placeholder=" Вопрос" class="form-control" required>
