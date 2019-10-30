348b91625efc3eec2ea80d2039705997d6ed63cf
Bronyrus <bronyruschannel@gmail.com>
2019-10-30 13:28:19 +0300

    Update PollWebController.php

--- app/Http/Controllers/Web/PollWebController.php
+++ app/Http/Controllers/Web/PollWebController.php
@@ -45,7 +45,7 @@ class PollWebController extends Controller
      */
     public function store(Request $request)
     {
-        dd($request->all_data);
+        dd($request->all_data[6]);
         // Poll::create([
         //     'uuid' => ,
         //     'name' => ,
