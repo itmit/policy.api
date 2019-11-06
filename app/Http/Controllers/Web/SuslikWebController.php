36c3831ecf54ac473e837219d11a08a952e69477
Bronyrus <bronyruschannel@gmail.com>
2019-11-06 11:03:40 +0300

    Update SuslikWebController.php

--- app/Http/Controllers/Web/SuslikWebController.php
+++ app/Http/Controllers/Web/SuslikWebController.php
@@ -304,7 +304,14 @@ class SuslikWebController extends Controller
         $path = storage_path() . '/app/susliks_upload';
         if (file_exists($path)) {
             foreach (glob($path.'/*') as $file) {
-                unlink($file);
+                if(is_dir($file))
+                {
+                    rmdir($file);
+                }
+                else
+                {
+                    unlink($file);
+                }
             }
         }
         return true;
