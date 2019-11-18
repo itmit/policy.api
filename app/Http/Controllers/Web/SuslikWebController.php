095c5183865ac720cb4ca730c599d3e0db0c08b4
Bronyrus <bronyruschannel@gmail.com>
2019-11-18 13:34:42 +0300

    Update SuslikWebController.php

--- app/Http/Controllers/Web/SuslikWebController.php
+++ app/Http/Controllers/Web/SuslikWebController.php
@@ -348,13 +348,19 @@ class SuslikWebController extends Controller
         $day = date("Y-m-d H:i:s");
         $inSevenDays =  date('Y-m-d H:i:s', strtotime('-1 week'));
         $lastSevenDays = SuslikRatingHistory::where('whom_suslik', '=', $id)->whereBetween('created_at', [$inSevenDays, $today])->get(); // все голоса за последние 7 дней
-        $i = 0;
+        $i = 1;
         $votes = [];
-        while ($i <= 6) {
+        while ($i <= 7) {
             $key = date("d.m.Y", strtotime($day));
             $votes[$key] = SuslikRatingHistory::where('whom_suslik', '=', $id)->where('created_at', '=', $day)->get();
-            $day = date("d.m.Y", strtotime('-1 day'));
-            return $day;
+            if($i == 1)
+            {
+                $day = date("d.m.Y", strtotime('-'.$i.' day'));
+            }
+            else
+            {
+                $day = date("d.m.Y", strtotime('-'.$i.' days'));
+            }
             $i++;
         }
 
