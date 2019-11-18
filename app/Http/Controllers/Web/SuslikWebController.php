43402aed40727e9d491bc010c6a995007289e317
Bronyrus <bronyruschannel@gmail.com>
2019-11-18 12:12:34 +0300

    Update SuslikWebController.php

--- app/Http/Controllers/Web/SuslikWebController.php
+++ app/Http/Controllers/Web/SuslikWebController.php
@@ -344,7 +344,7 @@ class SuslikWebController extends Controller
      */
     public function showStatistic($id)
     {
-        $lastSevenDays = SuslikRatingHistory::where('whom_suslik', '=', $id)->orderBy('created_at', 'desc')->get();
+        $lastSevenDays = SuslikRatingHistory::where('whom_suslik', '=', $id)->orderBy('created_at', 'desc')->limit(7)->get();
         dd($lastSevenDays);
         return view('statistic', [
             'suslik' => Suslik::where('id', '=', $id)->first(),
