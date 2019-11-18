ba79b256d7a0da7ee083f01b7e065d48227a5ac8
Bronyrus <bronyruschannel@gmail.com>
2019-11-18 16:40:48 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -38,6 +38,12 @@ Route::group(['middleware' => 'auth:api'], function() {
     Route::get('statistic/{uuid}', 'Api\SuslikApiController@showStatistic')->name('showStatistic');
 });
 
+// Password Reset Routes...
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+
 Route::post('poll/getPollCategoryList', 'Api\PollApiController@getPollCategoryList');
 Route::post('poll/getPollList', 'Api\PollApiController@getPollList');
 Route::post('poll/getPollQuestionList', 'Api\PollApiController@getPollQuestionList');
