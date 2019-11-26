011f43bb8185c06d7b473b363dcc566f2dd35b9f
Bronyrus <bronyruschannel@gmail.com>
2019-11-26 12:03:32 +0300

    Update pollDetail.blade.php

--- resources/views/polls/pollDetail.blade.php
+++ resources/views/polls/pollDetail.blade.php
@@ -1,1 +1,11 @@
-{{$poll->name}}
+@extends('layouts.adminApp')
+
+@section('content')
+<div class="col-sm-9 tabs-content">
+    <h1>Опрос {{ $poll->name }}</h1>
+<div class="row">
+    
+</div>
+
+</div>
+@endsection
