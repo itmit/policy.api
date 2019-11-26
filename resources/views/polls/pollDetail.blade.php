f095980e71b78880a4654b5a6be6225ac233ce29
Bronyrus <bronyruschannel@gmail.com>
2019-11-26 12:22:36 +0300

    Update pollDetail.blade.php

--- resources/views/polls/pollDetail.blade.php
+++ resources/views/polls/pollDetail.blade.php
@@ -6,12 +6,22 @@
 <div class="row">
     <div class="col-sm-12">
         <div class="form-group">
+            <label for="created_at" class="control-label">Создан: </label>
+            <div id="created_at">{{ $poll->created_at }}</div>
+        </div>
+        @if($poll->start_at != NULL && $poll->end_at != NULL)
+            <div class="form-group">
+                <label for="description" class="control-label">Дата начала и завершения: </label>
+                <div id="description">{{ $poll->start_at }} {{ $poll->end_at }}</div>
+            </div>
+        @endif
+        <div class="form-group">
             <label for="description" class="control-label">Описание: </label>
             <div id="description">{{ $poll->description }}</div>
         </div>
         <div class="form-group">
-            <label for="description" class="control-label">Категория: </label>
-            <div id="description">{{ $poll->category()->name }}</div>
+            <label for="category_name" class="control-label">Категория: </label>
+            <div id="category_name">{{ $poll->category()->name }}</div>
         </div>
 
     </div>
