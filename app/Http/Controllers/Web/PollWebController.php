1fa3adb0c09b7bfc7d9674a3b1ac9e19ba62d723
Bronyrus <bronyruschannel@gmail.com>
2019-11-26 12:09:44 +0300

    Update PollWebController.php

--- app/Http/Controllers/Web/PollWebController.php
+++ app/Http/Controllers/Web/PollWebController.php
@@ -131,10 +131,9 @@ class PollWebController extends Controller
      */
     public function show($id)
     {
-        return $id;
         return view('polls.pollDetail', [
             'poll' => Poll::where('id', '=', $id)
-            ->first('created_at', 'desc')
+            ->first()
         ]); 
     }
 
