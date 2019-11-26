a1ca438a8c331aff377b08ed25ec87153b7df5a3
Bronyrus <bronyruschannel@gmail.com>
2019-11-26 12:13:38 +0300

    Update pollDetail.blade.php

--- resources/views/polls/pollDetail.blade.php
+++ resources/views/polls/pollDetail.blade.php
@@ -4,7 +4,13 @@
 <div class="col-sm-9 tabs-content">
     <h1>Опрос {{ $poll->name }}</h1>
 <div class="row">
-    
+    <div class="col-sm-12">
+        <div class="form-group">
+            <label for="description" class="control-label">Описание: </label>
+            <div id="description">{{ $poll->description }}</div>
+        </div>
+
+    </div>
 </div>
 
 </div>
