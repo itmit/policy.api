26681d61912449cd0a62ada286ad73e19a4db59a
Bronyrus <bronyruschannel@gmail.com>
2019-11-27 13:10:39 +0300

    Update UserToPoll.php

--- app/UserToPoll.php
+++ app/UserToPoll.php
@@ -18,6 +18,6 @@ class UserToPoll extends Model
 
     public function user()
     {
-        return $this->belongsTo(User::class, 'user_id')->first();
+        return $this->belongsTo(User::class, 'user_id', 'number')->first();
     }
 }
