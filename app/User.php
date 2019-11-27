54c9719f1bc1abc8204015acd2d6f593f73a2ab3
Bronyrus <bronyruschannel@gmail.com>
2019-11-27 12:52:54 +0300

    Update User.php

--- app/User.php
+++ app/User.php
@@ -43,6 +43,6 @@ class User extends Authenticatable
 
     public function region()
     {
-        return $this->hasOne(Region::class, 'region')->first();
+        return $this->hasOne(Region::class, 'id')->first();
     }
 }
