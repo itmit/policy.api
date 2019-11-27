0983f179d62fa99db79d512e468311795d668738
Bronyrus <bronyruschannel@gmail.com>
2019-11-27 12:50:32 +0300

    Update pollDetail.blade.php

--- resources/views/polls/pollDetail.blade.php
+++ resources/views/polls/pollDetail.blade.php
@@ -73,11 +73,11 @@
                     <td>{{ $item->user()->id }}</td>
                     <td>{{ $item->user()->name }}</td>
                     @foreach($item->user()->userAnswer() as $answer)
-
+                    <?php $flag=0;?>
                         @foreach ($response as $key => $value)
                             
                             @foreach ($value['answers'] as $item)
-                            <?php $flag=0;?>
+                            
                                 @if($item['answer_id'] == $answer->answer_id)
                                     <td>1</td>
                                     <?php $flag=1;?>
