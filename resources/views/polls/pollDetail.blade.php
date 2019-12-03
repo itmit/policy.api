02a16d86b913d13813af26a607a0dde0dfaabfe6
Bronyrus <bronyruschannel@gmail.com>
2019-12-03 11:33:59 +0300

    Update pollDetail.blade.php

--- resources/views/polls/pollDetail.blade.php
+++ resources/views/polls/pollDetail.blade.php
@@ -82,7 +82,7 @@
                     @if($item->user()->region() == NULL)
                     <td></td>
                     @else
-                    <td>{{ $item->user()->region }}</td>
+                    <td>{{ $item->user()->region()->number }}</td>
                     @endif
                     <td>{{ $item->user()->id }}</td>
                     <td>{{ $item->user()->name }}</td>
