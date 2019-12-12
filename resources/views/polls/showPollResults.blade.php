7fcebfd9fbdad6c05e47de9676f90ea576a6ee92
Bronyrus <bronyruschannel@gmail.com>
2019-12-12 10:58:47 +0300

    Update showPollResults.blade.php

--- resources/views/polls/showPollResults.blade.php
+++ resources/views/polls/showPollResults.blade.php
@@ -1,5 +1,3 @@
-@extends('layouts.adminApp')
-
 @section('content')
 <div class="col-sm-9 tabs-content">
     <h1>Опрос {{ $poll->name }}</h1>
@@ -40,10 +38,143 @@
 </div>
 <div class="row">
     <div class="col-sm-12">
+            @if($data->count() == 0)
+            <tr>В данном опросе еще никто не принял участие</tr>
+            @else
             <?php $i=1;
             $sergay = [];
             $s=[];
             ?>
+            <table class="table policy-table" style="text-align: center; display:none">
+                <thead>
+                    <tr>
+                        <th scope="col">№</th>
+                        <th scope="col">пол (1-м, 2-ж)</th>
+                        <th scope="col">Год рождения</th>
+                        <th scope="col">Образование (1-высшее, 2-ср.специальное, 3-среднее полное, 4-неполное среднее, 5-начальное)</th>
+                        <th scope="col">Субъект РФ</th>
+                        <th scope="col">ID</th>
+                        <th scope="col">Имя/ник</th>
+                        <?php $i=1;?>
+                        @foreach ($response as $key => $value)
+                            <?php $y=1;?>
+                            @foreach ($value['answers'] as $item)
+                            <th scope="col">V{{$i}}_{{$y}}</th>
+                                <?php $y++;?>
+                            @endforeach
+                            <?php $i++;?>
+                        @endforeach
+                    </tr>
+                </thead>
+                <tbody>
+                @foreach ($response as $key => $value)
+                    @foreach ($value['answers'] as $a)
+                        <?php $sergay[$a['answer_id']] = 0;?>
+                    @endforeach
+                @endforeach
+                @foreach($data as $item)
+                    <tr>
+                        <td>{{ $i }}</td>
+                        @if($item->user()->sex == 'мужской')
+                        <td>1</td>
+                        @else
+                        <td>0</td>
+                        @endif
+                        <td>{{ date('Y', strtotime($item->user()->birthday)) }}</td>
+                        @if($item->user()->education == 'высшее или неполное высшее')
+                        <td>1</td>
+                        @elseif($item->user()->education == 'среднее (профессиональное)')
+                        <td>2</td>
+                        @elseif($item->user()->education == 'среднее (полное)')
+                        <td>3</td>
+                        @elseif($item->user()->education == 'среднее (общее)')
+                        <td>4</td>
+                        @elseif($item->user()->education == 'начальное')
+                        <td>5</td>
+                        @endif
+                        @if($item->user()->region() == NULL)
+                        <td></td>
+                        @else
+                        <td>{{ $item->user()->region()->id }}</td>
+                        @endif
+                        <td>{{ $item->user()->id }}</td>
+                        <td>{{ $item->user()->name }}</td>
+                        
+
+                        @foreach ($response as $key => $value)
+                            @foreach ($value['answers'] as $a)
+                                <?php $flag=0;?>
+                                
+                                @foreach($item->user()->userAnswer() as $answer)
+                                
+                                    @if($a['answer_id'] == $answer->answer_id)
+                                    <td>1</td>
+                                        <?php $flag=1;
+                                        $sergay[$a['answer_id']] = $sergay[$a['answer_id']] + 1;
+                                        ?>
+                                    @endif
+                                    
+                                @endforeach
+
+                                @if($flag==0)
+                                    <td>0</td>
+                                @endif
+
+                            @endforeach
+
+                        @endforeach
+    
+                    </tr>
+                <?php $i++;?>
+                @endforeach
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td>Итог (чел.):</td>
+                        <td></td>
+                        @foreach ($response as $key => $value)
+                            @foreach ($value['answers'] as $item)
+                            <td>{{ $sergay[$item['answer_id']] }}</td>
+                            @endforeach
+                        @endforeach
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td>К-во респондентов</td>
+                        <td></td>
+                        <td>Итог (%):</td>
+                        <td></td>
+                        @foreach ($response as $key => $value)
+                            @foreach ($value['answers'] as $item)
+                            <?php $percent = $sergay[$item['answer_id']] / $data->count() * 100 ?>
+                            <td>{{ round($percent, 1) }}</td>
+                            @endforeach
+                        @endforeach
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td>Чел</td>
+                        <td>%%</td>
+                        <td>Источник</td>
+                        <td>{{ $poll->link }}</td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td>{{$data->count()}}</td>
+                        <td>100%</td>
+                        <td>100%</td>
+                        <td>стр. {{ $poll->page }}</td>
+                    </tr>
+                </tbody>
+            </table>
+
         <?php $i=1;?>
         @foreach($response as $key => $value)
         <table class="table policy-table">
@@ -84,6 +215,7 @@
         </table>
         <?php $i++;?>
         @endforeach
+        @endif
     </div>
 </div>
 @endsection
