252f4749ef6702cb253ee29d5728907fd38b8888
Bronyrus <bronyruschannel@gmail.com>
2020-02-17 09:55:35 +0300

    Update SuslikWebController.php

--- app/Http/Controllers/Web/SuslikWebController.php
+++ app/Http/Controllers/Web/SuslikWebController.php
@@ -44,7 +44,7 @@ class SuslikWebController extends Controller
             'categories' => SusliksCategory::select('*')
             ->orderBy('created_at', 'desc')->get(),
             'regions' => Region::select('*')
-            ->orderBy('number', 'asc')->get()
+            ->orderBy('id', 'asc')->get()
         ]); 
     }
 
