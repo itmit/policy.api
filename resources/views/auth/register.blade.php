1bb4dbfa7fff10fc3dc15d1f0588b4d141823c3a
Bronyrus <bronyruschannel@gmail.com>
2020-03-26 17:44:21 +0300

    fix

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -47,20 +47,6 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
-                            <label for="phone" class="col-md-4 col-form-label text-md-right">{{ __('Дата рождения') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="birthday" type="date" class="form-control @error('birthday') is-invalid @enderror" name="birthday" value="{{ old('birthday') }}" required autocomplete="birthday">
-
-                                @error('birthday')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
