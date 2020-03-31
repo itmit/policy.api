1442464496abb748c3ed0bd49522a500a9db7363
Bronyrus <bronyruschannel@gmail.com>
2020-03-31 16:20:50 +0300

    Update SuslikWebController.php

--- app/Http/Controllers/Web/SuslikWebController.php
+++ app/Http/Controllers/Web/SuslikWebController.php
@@ -89,7 +89,7 @@ class SuslikWebController extends Controller
         // dd($request);
         $uuid = (string) Str::uuid();
         $file = $request->file('photo');
-        dd($request->file('photo'));
+        // dd($request->file('photo'));
         $path = storage_path() . '/app/' . $file->store('susliks_upload');
         Storage::putFileAs(storage_path() . '/app/public/susliks/', new File($path), $uuid.'.jpg');
 
