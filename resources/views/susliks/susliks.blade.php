af642608ecd8c840a0c682cbec2d91a193f37ccf
Bronyrus <36816185+Bronyrus@users.noreply.github.com>
2020-04-01 11:50:38 +0300

    Update susliks.blade.php

--- resources/views/susliks/susliks.blade.php
+++ resources/views/susliks/susliks.blade.php
@@ -255,7 +255,6 @@ $(document).ready(function() {
                     default:
                         break;
                 }
-                $('th').data('d', 0);
                 $('.loader').css('display', 'none');
             },
             error: function (xhr, err) { 
